c044b8b2acbd1d0b8601fa7a2981838a8fc7225b
Paulo Chiozzani <chiozzani23@icloud>
2015-09-17 20:02:15 -0300

    operação somar

--- calculadora.php
+++ calculadora.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<Title> Calculadora</title>
 
+  <div id="calculadora">
+    <form id="Resultado" method="post">
+     <p><label> N1 </label><input name="inptN1"></input></p>
+     <p><label>Operacao</label>
+        <select name="operacao">
+             <option id="optAdd" value="adicao">Adicao</option>
+        </select>    
+     </p>
+     <p><label>N2</label><input name="iptN2"></input></p>
+     <p><label> Resultado:</label></p>
+     <p><input type="submit" id="btnCalc" value="Calcular"></p>
+     </form>
+   </div>  
+</body>
+</html>
